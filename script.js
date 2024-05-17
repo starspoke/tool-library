16f5466b0cdfe88deafe8140e97e8cfe246dcc8d
starspoke <oscarjsill@gmail.com>
2024-05-17 23:56:09 +0100

    Mobile specific CSS + other minor CSS formatting

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 function displayHeader(pageTitle) {
-    document.getElementById("pageheader").innerHTML = "<h2>" + pageTitle +
-        "</h2><p><a href='index.html'>Home</a>&nbsp;&nbsp;" +
-        "<a href='ideas.html'>Ideas</a>&nbsp;&nbsp;" +
-        "<a href='add.html'>Add an item</a>&nbsp;&nbsp;" +
-        "<a href='browse.html'>Browse the library</a>&nbsp;&nbsp;" +
-        "<a href='signout.html'>Sign out</a>&nbsp;&nbsp;</p>";
+    document.getElementById("pageheader").innerHTML = "<h2 id='headTitle'>" + pageTitle +
+        "</h2><p><a class='headLink' href='index.html'>Home</a>&nbsp;&nbsp;" +
+        "<a class='headLink' href='ideas.html'>Ideas</a>&nbsp;&nbsp;" +
+        "<a class='headLink' href='add.html'>Add an item</a>&nbsp;&nbsp;" +
+        "<a class='headLink' href='browse.html'>Browse the library</a>&nbsp;&nbsp;" +
+        "<a class='headLink' href='signout.html'>Sign out</a>&nbsp;&nbsp;</p>";
 }
 
 function displaySignedOutHeader(pageTitle) {
